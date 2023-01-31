433900506ef5f7be4d2518d2ccb2a238c3c2a5b0
qospwmf <qospwmf@gmail.com>
2023-01-31 15:15:22 +0900

    [spring-container] 스프링 컨테이너 생성 뒤 컨트롤러 빈 등록후 프론트 컨트롤러에서 중요 로직 동작 스프링 컨테이너에게 위임

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,6 +2,7 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,10 +16,14 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh();
+
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
-			HelloController helloController = new HelloController();
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,6 +34,8 @@ public class HellobootApplication {
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 
 						String name = req.getParameter("name");
+
+						HelloController helloController = applicationContext.getBean(HelloController.class);
 
 						String returnVal = helloController.hello(name);
 
